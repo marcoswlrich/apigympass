4ae5e0fcab90461c5fcd4dae22bd7adab2780e6b
marcoswlrich <marcosapw.dev@gmail.com>
2023-11-01 00:47:29 -0400

    Add In Memory Db Register Users

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,24 +1,13 @@
 import { describe, expect, it } from "vitest";
 import { RegisterUseCase } from "./register";
 import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 describe("Register Use Case", () => {
-  it("should hash user password upon registration", async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null;
-      },
-
-      async create(data) {
-        return {
-          id: "user-1",
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        };
-      },
-    });
+  it("should to register", async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(userRepository);
 
     const { user } = await registerUseCase.execute({
       name: "Jonh Doe",
@@ -26,11 +15,26 @@ describe("Register Use Case", () => {
       password: "123456",
     });
 
-    const isPasswordCorrectlyHashed = await compare(
-      "123456",
-      user.password_hash,
-    );
+    expect(user.id).toEqual(expect.any(String));
+  });
+  it("should not be able to register with same email twice", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
 
-    expect(isPasswordCorrectlyHashed).toBe(true);
+    const email = "johndoe@example.com";
+
+    await registerUseCase.execute({
+      name: "John Doe",
+      email,
+      password: "123456",
+    });
+
+    expect(() =>
+      registerUseCase.execute({
+        name: "John Doe",
+        email,
+        password: "123456",
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });
